fa3917e4689a0c07b6f9c57db81cedb4c9b89d52
pauligree <pauligree@gmail.com>
2017-01-17 17:29:17 +0100

    aggiunto opuscolo

---
+++ index-opuscolo.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="it">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Opuscolo">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<title>Opuscolo</title>
+
+	<!-- Add to homescreen for Chrome on Android -->
+	<meta name="mobile-web-app-capable" content="yes">
+	<link rel="icon" sizes="192x192" href="images/android-desktop.png">
+
+	<!-- Add to homescreen for Safari on iOS -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-title" content="q.b. Per sapere quanto basta ">
+	<link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
+
+	<!-- Tile icon for Win8 (144x144 + tile color) -->
+	<meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+	<meta name="msapplication-TileColor" content="#3372DF">
+
+	<link rel="shortcut icon" href="images/favicon.png">
+
+	<!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+    <!--
+    <link rel="canonical" href="http://www.example.com/">
+-->
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+
+<script  src="https://code.jquery.com/jquery-3.1.1.min.js"  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="  crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/mdl-jquery-modal-dialog.css">
+<script src="js/mdl-jquery-modal-dialog.js"></script>
+
+
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+		<header class="demo-header mdl-layout__header">
+			<div class="mdl-layout__header-row">
+				<span class="mdl-layout-title"><img src="images/q.b._Logo_web_orz.png" class="logo-orz-header" alt="q.b. per sapere quanto basta"/></span>
+				<div class="mdl-layout-spacer"></div>
+			</div>
+		</header>
+		<div class="demo-drawer mdl-layout__drawer mdl-color--grey-800 mdl-color-text--white" aria-hidden="false">
+			<header class="demo-drawer-header">
+				<img src="images/q.b._Logo_web_vert.png" class="logo-vrt-drawer" alt="q.b. per sapere quanto basta" />
+			</header>
+
+			<!--                       -->
+			<!--  MENU LATERALE        -->
+			<!--                       -->
+
+
+			<nav class="demo-navigation mdl-navigation mdl-color--grey-900">
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--white material-icons" role="presentation">people</i>Chi siamo</a>
+				<a id="open-modal-glossario" class="mdl-navigation__link" href="#"><i class="mdl-color-text--white material-icons" role="presentation">pageview</i>Glossario</a>
+				<div class="mdl-layout-spacer"></div>
+			</nav>
+		</div>
+
+
+		<!--                       -->
+		<!--        PAGINA         -->
+		<!--                       -->
+
+
+		<main class="mdl-layout__content mdl-color--grey-100">
+			<div class="mdl-grid demo-content">
+
+				<!--    CONTENUTO     -->
+
+
+				<div class="mdl-color--white- mdl-shadow--2dp- mdl-cell mdl-cell--12-col mdl-grid">
+					<div class="mdl-color--white- mdl-shadow--2dp- mdl-cell--9-col  mdl-grid">
+						<h2>Opuscolo</h2>
+						<p>Lo screening è un&rsquo;azione di sanità pubblica con la quale la popolazione sana, in età giudicata a rischio, è invitata dalla propria azienda sanitaria a sottoporsi gratuitamente ad esami preventivi (quali mammografia, pap-test e ricerca del sangue occulto fecale).<br /> Lo screening può permettere di anticipare la diagnosi e la cura del tumore, consentendo in genere interventi chirurgici più semplici. Per lo screening del tumore della mammella ogni donna tra i 50 e i 69 anni riceve una lettera di invito con appuntamento prefissato per effettuare una mammografia gratuita ogni 2 anni. La ricerca scientifica ha dimostrato che gli screening per i tumori della mammella, dell&rsquo;utero e del colon-retto sono in grado di salvare numerose vite umane.</p>
+						<h3>La Mammografia</h3>
+						<p>Attualmente, per le donne tra i 50 e i 69 anni, la mammografia è il test più efficace per individuare i tumori che non danno ancora sintomi e non sono palpabili.<br /> È una radiografia del seno che utilizza dosi di raggi X molto basse. Per ottenere un risultato chiaro è necessario comprimere le mammelle. Questa compressione può risultare fastidiosa o leggermente dolorosa, ma dura pochi secondi. Il personale che effettua la mammografia è altamente qualificato e le apparecchiature utilizzate sono tecnologicamente avanzate e controllate. Tutto ciò permette di ottenere immagini di ottima qualità.</p>
+						<p>Per una maggiore sicurezza sulla diagnosi, le immagini sono esaminate separatamente da due radiologi per garantire una maggiore precisione. Tuttavia, come tutti gli esami, non è infallibile: in alcuni casi può non riconoscere un tumore che c&rsquo;è o fare sospettare un tumore inesistente.</p>
+						<h3>Perche&rsquo; Fare La Mammografia</h3>
+						<p>In Italia i tumori della mammella sono al primo posto tra le donne per incidenza, cioè per nuovi casi l'anno. E&rsquo; il tumore più frequente fra le donne, con 36.000 nuovi casi e 11.000 decessi all&rsquo;anno. La mammografia eseguita regolarmente permette di effettuare una diagnosi precoce, prima dell'insorgenza dei sintomi, e di poter intervenire sul tumore con elevate probabilità di guarigione definitiva. I dati indicano che, per ogni 1000 donne di età tra i 50 e i 69 anni che eseguono regolarmente la mammografia, nell&rsquo;arco di 20 anni si possono salvare 7-9 vite.</p>
+						<h3>Quali Sono I Limiti/svantaggi Della Mammografia?</h3>
+						<ul>
+							<li>In genere ogni 100 donne che effettuano una mammografia, circa 5 devono ripetere l&rsquo;esame o sottoporsi ad approfondimenti. Per 4 di loro si tratta in realtà di &lsquo;falsi allarmi&rsquo;, in</li>
+							<li>quanto gli approfondimenti escludono la presenza di un tumore (i cosiddetti falsi positivi). Lo svantaggio è rappresentato principalmente dall'ansia che una donna può provare in tali situazioni.</li>
+							<li>Non sempre la mammografia è in grado di evidenziare il tumore quando è presente. In alcuni casi la mammella è molto densa (cioè contiene molte ghiandole) e ciò rende difficile l&rsquo;identificazione del tumore (i cosiddetti falsi negativi).</li>
+							<li>Ci sono casi in cui la malattia si sviluppa molto rapidamente nell&rsquo;intervallo tra due test di screening (i cosiddetti cancri di intervallo). È quindi importante prestare sempre attenzione ai cambiamenti del seno come indurimenti, cambiamenti della pelle o fuoriuscita di liquido dal capezzolo e alla presenza di noduli ascellari, anche nel caso in cui la mammografia non abbia evidenziato anomalie. Se si notano delle anomalie è opportuno rivolgersi subito al proprio medico.</li>
+							<li>Alcune anomalie, tra quelle che vengono rilevate dalla mammografia, non sono destinate a trasformarsi in tumori invasivi e a compromettere la salute della donna. In questi casi (1 caso di tumore ogni 10 diagnosticati) si può incorrere in interventi che non sarebbero stati necessari (la cosidetta sovradiagnosi). Questo purtroppo è inevitabile in quanto le conoscenze attuali non consentono di distinguere fin dall&rsquo;inizio tra i tumori che sono una minaccia per la vita e quelli che non lo sono.</li>
+						</ul>
+						<h3>Quale Può Essere L&rsquo;esito Della Mammografia?</h3>
+						<ul>
+							<li>Se non risulta nulla di sospetto, riceverà una lettera di esito negativo.<br /> È bene sapere che lo scopo dello screening mammografico è individuare tumori maligni riconoscibili ai raggi x; non vengono considerate altre patologie della mammella.</li>
+							<li>Se si evidenziano immagini dubbie, sarà contattata ed invitata a recarsi al centro di screening per eseguire approfondimenti, che possono comprendere altre mammografie, ecografia o talvolta un piccolo prelievo di tessuto mammario (biopsia). La maggior parte di questi casi dubbi non è dovuta alla presenza di un tumore, ma è necessario eseguire questi esami supplementari proprio per esserne sicuri.</li>
+						</ul>
+					</div>
+				</div>
+			</main>
+		</div>
+
+
+     <!--    MODALS      -->
+      <!--    mettere tutti i modal dentro il div hide-modal per rimuoverli dalla visualizzazione  -->
+
+      <div class="hide-modal">
+
+        <?php include'includes/common_modals.php';?>
+        
+
+      </div>
+
+		<!--    SCRIPTS DI CHIUSURA      -->
+
+
+		<script type="text/javascript" src="js/scripts.js"></script>
+		<script src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+		<script type="text/javascript">
+        $( document ).ready(function() {
+
+          // TUTTI I MODAL
+          
+          //copio l'html del modal nelle variabili da mostrare
+          var glossario = $('#modal-glossario').html();
+
+          $('#open-modal-glossario').click(function (e) {
+            e.preventDefault();
+            showDialog({
+              text: glossario,
+              positive: {
+                title: 'Chiudi'
+              }
+            });
+          });
+
+        });
+      </script>
+</body>
+</html>
